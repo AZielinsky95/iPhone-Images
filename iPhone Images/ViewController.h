9ba0e40c82e86025d84cf4e7bd1383b574b50633
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-26 11:13:06 -0400

    Initial Commit

---
+++ iPhone Images/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhone Images
+//
+//  Created by Alejandro Zielinsky on 2018-04-26.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPhone Images/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iPhone Images
+//
+//  Created by Alejandro Zielinsky on 2018-04-26.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
